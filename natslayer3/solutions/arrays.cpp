23fa7906bf5b15a15783c96194214d37dac38aff
Natalia Montserrat Cavazos Almeida <a01571724@tec.mx>
2026-01-09 01:24:21 -0600

    day4

--- natslayer3/solutions/arrays.cpp
+++ natslayer3/solutions/arrays.cpp
@@ -148,3 +148,76 @@ public:
         return ans;
     }
 };
+
+/*
+238. Product of Array Except Self M
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+*/
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        int countZ = 0;
+        vector<int> answer(n, 0);
+        int prod = 1;
+
+        for (int i = 0; i < n; i++){
+            if (nums[i] != 0){
+                prod = prod*nums[i];
+            }
+            else {
+                countZ++;
+            }
+        }
+
+        if (countZ > 1){
+            return answer;
+        }
+
+        for (int i = 0; i < n; i++){
+            if (countZ > 0) {
+                answer[i] = (nums[i] == 0) ? prod : 0;
+            }
+            else{
+                answer[i] = prod / nums[i];
+            }
+        }
+        return answer;   
+    }
+};
+
+/*
+
+        if (nums[0] == '0'){
+            countZ++;
+        }
+
+        for (int i = 1; i < n; i++){
+            if (nums[i] == '0'){
+                countZ++;
+                int pos = i;
+            }
+            prefixM[i] = prefixM[i-1] * nums[i];
+
+
+
+
+            if (countZ >= '2'){
+                bool zer = true; 
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            answer[i] = prefixM[n-1] / prefixM[i];
+        }
+        */
+
+
+
+
